56489967c4b2f5eaa5665bd757671f4d26c2d8ef
arebrov <arebrov@alex.(none)>
2015-11-19 23:14:49 +0200

    added example of parsing

--- RESTClient/client.py
+++ RESTClient/client.py
@@ -6,8 +6,12 @@ Created on Nov 19, 2015
 import requests
 
 def send_get():
-    response = requests.get('https://api.github.com/events');
-    if response.status_code==200:
-        print response.json();  
+    response = requests.get('http://jsonplaceholder.typicode.com/users')
+    if response.status_code == 200:
+        print response.headers
+        for jsonObject in response.json():
+        #work with json as a dictionary
+            print jsonObject['id']
+            print jsonObject
 
 send_get();
