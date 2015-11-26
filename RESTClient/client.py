754b4e65379d332466f3d654820029e91fe8173f
arebrov <rebrovoleksii@gmail.com>
2015-11-26 19:14:37 +0200

    some prototype to check tickets from website

--- RESTClient/client.py
+++ RESTClient/client.py
@@ -4,14 +4,32 @@ Created on Nov 19, 2015
 @author: arebrov
 '''
 import requests
+from lxml import html,etree
+
+URL = 'https://dprc.gov.ua/show.php?transport_type=2&src=22204001&dst=22218000&dt=2015-12-30&ret_dt=2001-01-01&ps=ec_privat'
+#  URL = ''http://jsonplaceholder.typicode.com/users''
 
 def send_get():
-    response = requests.get('http://jsonplaceholder.typicode.com/users')
+    response = requests.get(URL)
     if response.status_code == 200:
         print response.headers
         for jsonObject in response.json():
-        #work with json as a dictionary
+            # work with json as a dictionary
             print jsonObject['id']
             print jsonObject
 
-send_get();
+def send_get_plain():
+    response = requests.get(URL)
+    if response.status_code == 200:
+        #print response.headers
+        #print response.content
+        html_tree = html.fromstring(response.content)
+        print html_tree
+        expr = "//*[@class=$name]"
+        trains = html_tree.xpath(expr,name="train_row")
+        for train in trains:
+            for el in train.iter():
+                print el.text
+
+
+send_get_plain()
